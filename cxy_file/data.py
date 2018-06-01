c50faaf2c822474de4293e6655cf8048abdd740b
Jokevil <476686596@qq.com>
2018-06-01 17:06:16 +0800

    add cxy file

---
+++ cxy_file/data.py
@@ -0,0 +1,323 @@
+{
+	u'report_time': 1526892316000 L,
+	  u'risk_items': 
+	  [
+	  {   
+		u'item_detail': {
+			u'namelist_hit_details':  
+			[{
+				u'type': u'black_list',
+				u'court_details': 
+				[{
+				    u'province': u'山西',
+					u'duty': u'被告于本判决生效后10日内偿还原告借款10000元并支付相应利息',
+					u'execution_department': u'运城市人民法院',
+					u'name': u'赵六',
+					u'situation': u'全部未履行',
+					u'gender': u'男',
+					u'age': u'27',
+					u'execution_base': u'(2017)榆民二初字第112号民事判决书',
+					u'court_name': u'运城市人民法院',
+					u'filing_time': u'2010年1月4日',
+					u'case_number': u'(2010)榆执字第0121234号',
+					u'discredit_detail': u'其他有履行能力而拒不履行生效法律文书确定义务',
+					u'fraud_type': u'法院失信'
+				}, {
+					u'province': u'山西',
+					u'duty': u'被告于本判决生效后10日内偿还原告借款10000元并支付相应利息',
+					u'execution_department': u'运城市人民法院',
+					u'name': u'赵六',
+					u'situation': u'全部未履行',
+					u'gender': u'男',
+					u'age': u'27',
+					u'execution_base': u'(2017)榆民二初字第112号民事判决书',
+					u'court_name': u'运城市人民法院',
+					u'filing_time': u'2010年1月4日',
+					u'case_number': u'(2010)榆执字第0121234号',
+					u'discredit_detail': u'其他有履行能力而拒不履行生效法律文书确定义务',
+					u'fraud_type': u'法院失信'
+				}],
+				u'hit_type_displayname': u'借款人身份证',
+				u'description': u'身份证命中法院失信名单',
+				u'fraud_type': u'法院失信、法院失信'
+			}],
+			u'fraud_type': u'法院失信、法院失信'
+		},
+		u'item_id': 3763711,
+		u'risk_level': u'high',
+		u'group': u'风险信息扫描',
+		u'item_name': u'身份证命中法院失信名单'
+	}, {
+		u'item_detail': {
+			u'namelist_hit_details': 
+			[{
+				u'type': u'black_list',
+				u'court_details': [{
+					u'name': u'赵六',
+					u'execution_status': u'-',
+					u'filing_time': u'2010年1月4日',
+					u'case_number': u'(2010)榆执字第0121234号',
+					u'court_name': u'运城市人民法院',
+					u'execution_number': u'-',
+					u'fraud_type': u'法院执行'
+				}, {
+					u'name': u'赵六',
+					u'execution_status': u'-',
+					u'filing_time': u'2010年1月4日',
+					u'case_number': u'(2010)榆执字第0121234号',
+					u'court_name': u'运城市人民法院',
+					u'execution_number': u'-',
+					u'fraud_type': u'法院执行'
+				}, {
+					u'name': u'赵六',
+					u'execution_status': u'-',
+					u'filing_time': u'2010年1月4日',
+					u'case_number': u'(2010)榆执字第0121234号',
+					u'court_name': u'运城市人民法院',
+					u'execution_number': u'-',
+					u'fraud_type': u'法院执行'
+				}],
+				u'hit_type_displayname': u'借款人身份证',
+				u'description': u'身份证命中法院执行名单',
+				u'fraud_type': u'法院执行、法院执行、法院执行'
+			}],
+			u'fraud_type': u'法院执行、法院执行、法院执行'
+		},
+		u'item_id': 3763715,
+		u'risk_level': u'high',
+		u'group': u'风险信息扫描',
+		u'item_name': u'身份证命中法院执行名单'
+	}, {
+		u'item_detail': {
+			u'namelist_hit_details': [{
+				u'type': u'black_list',
+				u'court_details': [{
+					u'province': u'山西',
+					u'duty': u'被告于本判决生效后10日内偿还原告借款10000元并支付相应利息',
+					u'execution_department': u'运城市人民法院',
+					u'name': u'赵六',
+					u'situation': u'全部未履行',
+					u'gender': u'男',
+					u'age': u'27',
+					u'execution_base': u'(2017)榆民二初字第112号民事判决书',
+					u'execution_status': u'-',
+					u'filing_time': u'2010年1月4日',
+					u'case_number': u'(2010)榆执字第0121234号',
+					u'discredit_detail': u'其他有履行能力而拒不履行生效法律文书确定义务',
+					u'court_name': u'运城市人民法院',
+					u'execution_number': u'-',
+					u'fraud_type': u'法院结案'
+				}, {
+					u'province': u'山西',
+					u'duty': u'被告于本判决生效后10日内偿还原告借款10000元并支付相应利息',
+					u'execution_department': u'运城市人民法院',
+					u'name': u'赵六',
+					u'situation': u'全部未履行',
+					u'gender': u'男',
+					u'age': u'27',
+					u'execution_base': u'(2017)榆民二初字第112号民事判决书',
+					u'execution_status': u'-',
+					u'filing_time': u'2010年1月4日',
+					u'case_number': u'(2010)榆执字第0121234号',
+					u'discredit_detail': u'其他有履行能力而拒不履行生效法律文书确定义务',
+					u'court_name': u'运城市人民法院',
+					u'execution_number': u'-',
+					u'fraud_type': u'法院结案'
+				}],
+				u'hit_type_displayname': u'借款人身份证',
+				u'description': u'身份证命中法院结案名单',
+				u'fraud_type': u'法院结案、法院结案、法院结案、法院结案'
+			}],
+			u'fraud_type': u'法院结案、法院结案、法院结案、法院结案'
+		},
+		u'item_id': 3763725,
+		u'risk_level': u'medium',
+		u'group': u'风险信息扫描',
+		u'item_name': u'身份证命中法院结案名单'
+	}, {
+		u'item_detail': {
+			u'namelist_hit_details': [{
+				u'type': u'black_list',
+				u'hit_type_displayname': u'借款人身份证',
+				u'description': u'身份证命中欠款公司法人代表名单',
+				u'fraud_type': u'欠款公司法人代表'
+			}],
+			u'fraud_type': u'欠款公司法人代表'
+		},
+		u'item_id': 3763735,
+		u'risk_level': u'high',
+		u'group': u'风险信息扫描',
+		u'item_name': u'身份证命中欠款公司法人代表名单'
+	}, {
+		u'item_detail': {
+			u'namelist_hit_details': [{
+				u'type': u'black_list',
+				u'hit_type_displayname': u'借款人手机',
+				u'description': u'手机号命中虚假号码库',
+				u'fraud_type': u'虚假号码'
+			}],
+			u'fraud_type': u'虚假号码'
+		},
+		u'item_id': 3763753,
+		u'risk_level': u'medium',
+		u'group': u'个人基本信息核查',
+		u'item_name': u'手机号命中虚假号码库'
+	}, {
+		u'item_detail': {
+			u'namelist_hit_details': [{
+				u'type': u'grey_list',
+				u'hit_type_displayname': u'借款人手机',
+				u'description': u'手机号命中低风险关注名单',
+				u'fraud_type': u'法院失信、法院执行'
+			}],
+			u'fraud_type': u'法院失信、法院执行'
+		},
+		u'item_id': 3763759,
+		u'risk_level': u'low',
+		u'group': u'风险信息扫描',
+		u'item_name': u'手机号命中高风险关注名单'
+	}, {
+		u'item_detail': {
+			u'overdue_details': [{
+				u'overdue_time': u'2016-03',
+				u'overdue_amount_range': u'(10000, 50000]',
+				u'overdue_day_range': u'(90, 180]',
+				u'overdue_count': 1
+			}],
+			u'type': u'discredit_count',
+			u'discredit_times': 1
+		},
+		u'item_id': 3763761,
+		u'risk_level': u'medium',
+		u'group': u'风险信息扫描',
+		u'item_name': u'手机号命中信贷逾期名单'
+	}, {
+		u'item_id': 3763765,
+		u'item_name': u'手机号疑似乱填',
+		u'risk_level': u'medium',
+		u'group': u'个人基本信息核查'
+	}, {
+		u'item_detail': {
+			u'frequency_detail_list': [{
+				u'data': [u'823※※418@qq.com'],
+				u'detail': u'3个月身份证关联邮箱数：1'
+			}, {
+				u'data': [u'13100000000', u'130※※※※0000', u'188※※※※0102', u'133※※※※3333', u'135※※※※2659'],
+				u'detail': u'3个月身份证关联手机号数：5'
+			}],
+			u'type': u'frequency_detail'
+		},
+		u'item_id': 3763835,
+		u'risk_level': u'low',
+		u'group': u'客户行为检测',
+		u'item_name': u'3个月内身份证关联多个申请信息'
+	}, {
+		u'item_detail': {
+			u'frequency_detail_list': [{
+				u'data': [u'520001190812121210', u'3412※※※※※※※※※※0759', u'2310※※※※※※※※※※121x', u'4203※※※※※※※※※※2569', u'e7f7※※※※※※※※※※※※※※※※※※※※※※※※cc09', u'4511※※※※※※※※※※5626', u'2106※※※※※※※※※※3434', u'4405※※※※※※※※※※1827', u'1504※※※※※※※※※※5891', u'3301※※※※※※※※※※4411', u'3701※※※※※※※※※※9996', u'3415※※※※※※※※※※9593'],
+				u'detail': u'3个月手机号关联身份证数：12'
+			}],
+			u'type': u'frequency_detail'
+		},
+		u'item_id': 3763839,
+		u'risk_level': u'low',
+		u'group': u'客户行为检测',
+		u'item_name': u'3个月内申请信息关联多个身份证'
+	}, {
+		u'item_id': 3763847,
+		u'item_name': u'3个月内申请人手机号作为联系人手机号出现的次数过多',
+		u'risk_level': u'low',
+		u'group': u'客户行为检测'
+	}, {
+		u'item_detail': {
+			u'frequency_detail_list': [{
+				u'detail': u'7天内手机号申请次数：60'
+			}],
+			u'type': u'frequency_detail'
+		},
+		u'item_id': 3763859,
+		u'risk_level': u'low',
+		u'group': u'客户行为检测',
+		u'item_name': u'7天内设备或身份证或手机号申请次数过多'
+	}, {
+		u'item_detail': {
+			u'platform_detail': [u'银行个人业务:1', u'直销银行:1', u'信用卡中心:2', u'小额贷款公司:1'],
+			u'platform_detail_dimension': [{
+				u'count': 5,
+				u'detail': [u'银行个人业务:1', u'直销银行:1', u'信用卡中心:2', u'小额贷款公司:1'],
+				u'dimension': u'借款人手机详情'
+			}, {
+				u'count': 4,
+				u'detail': [u'银行个人业务:1', u'信用卡中心:2', u'小额贷款公司:1'],
+				u'dimension': u'借款人身份证详情'
+			}],
+			u'type': u'platform_detail',
+			u'platform_count': 5
+		},
+		u'item_id': 3763893,
+		u'risk_level': u'high',
+		u'group': u'多平台借贷申请检测',
+		u'item_name': u'7天内申请人在多个平台申请借款'
+	}, {
+		u'item_detail': {
+			u'platform_detail': [u'银行个人业务:1', u'直销银行:1', u'信用卡中心:2', u'小额贷款公司:4', u'P2P网贷:1', u'房屋租赁:1'],
+			u'platform_detail_dimension': [{
+				u'count': 9,
+				u'detail': [u'银行个人业务:1', u'直销银行:1', u'信用卡中心:2', u'小额贷款公司:3', u'P2P网贷:1', u'房屋租赁:1'],
+				u'dimension': u'借款人手机详情'
+			}, {
+				u'count': 9,
+				u'detail': [u'银行个人业务:1', u'信用卡中心:2', u'小额贷款公司:4', u'P2P网贷:1', u'房屋租赁:1'],
+				u'dimension': u'借款人身份证详情'
+			}],
+			u'type': u'platform_detail',
+			u'platform_count': 10
+		},
+		u'item_id': 3763895,
+		u'risk_level': u'medium',
+		u'group': u'多平台借贷申请检测',
+		u'item_name': u'1个月内申请人在多个平台申请借款'
+	}, {
+		u'item_detail': {
+			u'platform_detail': [u'一般消费分期平台:2', u'担保:1', u'银行个人业务:3', u'银行消费金融公司:1', u'融资租赁:1', u'直销银行:1', u'信用卡中心:3', u'小额贷款公司:10', u'P2P网贷:5', u'房屋租赁:1'],
+			u'platform_detail_dimension': [{
+				u'count': 27,
+				u'detail': [u'一般消费分期平台:2', u'担保:1', u'银行个人业务:3', u'银行消费金融公司:1', u'融资租赁:1', u'直销银行:1', u'信用卡中心:3', u'小额贷款公司:9', u'P2P网贷:5', u'房屋租赁:1'],
+				u'dimension': u'借款人手机详情'
+			}, {
+				u'count': 26,
+				u'detail': [u'一般消费分期平台:2', u'担保:1', u'银行个人业务:3', u'银行消费金融公司:1', u'融资租赁:1', u'信用卡中心:3', u'小额贷款公司:9', u'P2P网贷:5', u'房屋租赁:1'],
+				u'dimension': u'借款人身份证详情'
+			}],
+			u'type': u'platform_detail',
+			u'platform_count': 28
+		},
+		u'item_id': 3763897,
+		u'risk_level': u'medium',
+		u'group': u'多平台借贷申请检测',
+		u'item_name': u'3个月内申请人在多个平台申请借款'
+	}, {
+		u'item_detail': {
+			u'platform_detail': [u'银行个人业务:1'],
+			u'type': u'platform_detail',
+			u'platform_count': 1
+		},
+		u'item_id': 3763899,
+		u'risk_level': u'medium',
+		u'group': u'多平台借贷负债检测',
+		u'item_name': u'3个月内申请人在多个平台被放款_不包含本合作方'
+	}, {
+		u'item_id': 3763829,
+		u'item_name': u'1个月内同一个设备或手机号申请被拒次数过多',
+		u'risk_level': u'medium',
+		u'group': u'客户行为检测'
+	}
+	], 
+	u'application_id': u'180521164515839BA3E2E9AF0C84C023', u'success': True, u'final_score': 100, 
+		u'apply_time': 1526892315000 L, 
+		u'address_detect': {
+		u'mobile_address': u'山西省太原市'
+	}, u'report_id': u'ER20180521164515DC22DCF8', u'final_decision': u'Reject'
+}
+
+
